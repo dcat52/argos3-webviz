1c2fa2d78b8f42d7e85b12a15e88015b537f9339
Davis <daviscatherman@gmail.com>
2026-04-12 21:19:38 -0400

    client-next: fix build for Node 18 (Tailwind v3, react-resizable-panels API)

--- client-next/src/ui/Layout.tsx
+++ client-next/src/ui/Layout.tsx
@@ -1,4 +1,4 @@
-import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels'
+import { Panel, Group as PanelGroup, Separator } from 'react-resizable-panels'
 import { Toolbar } from './Toolbar'
 import { Sidebar } from './Sidebar'
 import { LogPanel } from './LogPanel'
@@ -6,7 +6,7 @@ import { Scene } from '../scene/Scene'
 
 function ResizeHandle({ direction }: { direction: 'horizontal' | 'vertical' }) {
   return (
-    <PanelResizeHandle
+    <Separator
       className={`${
         direction === 'horizontal' ? 'w-1' : 'h-1'
       } bg-white/5 hover:bg-blue-500/40 transition-colors`}
@@ -19,21 +19,21 @@ export function Layout() {
     <div className="h-screen flex flex-col overflow-hidden">
       <Toolbar />
       <PanelGroup direction="horizontal" className="flex-1">
-        <Panel defaultSize={75} minSize={40}>
+        <Panel defaultSize={75} min={40}>
           <PanelGroup direction="vertical">
-            <Panel defaultSize={75} minSize={30}>
+            <Panel defaultSize={75} min={30}>
               <div className="relative w-full h-full">
                 <Scene />
               </div>
             </Panel>
             <ResizeHandle direction="vertical" />
-            <Panel defaultSize={25} minSize={10}>
+            <Panel defaultSize={25} min={10}>
               <LogPanel />
             </Panel>
           </PanelGroup>
         </Panel>
         <ResizeHandle direction="horizontal" />
-        <Panel defaultSize={25} minSize={15}>
+        <Panel defaultSize={25} min={15}>
           <Sidebar />
         </Panel>
       </PanelGroup>
