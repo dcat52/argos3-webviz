329ffe5aad90299df6af23c153401cb993018458
Davis <daviscatherman@gmail.com>
2026-04-14 22:18:56 -0400

    PN-007: Leo entity renderer
    - Create LeoRenderer.tsx (cylinder body r=0.30, h=0.12, direction indicator)
    - Register as 'Leo' type in entity registry
    - Supports overrideColor, selection highlight, sensor rays
    - No LEDs (Leo has no CLEDEquippedEntity)

--- client-next/src/entities/renderers/index.ts
+++ client-next/src/entities/renderers/index.ts
@@ -1,6 +1,7 @@
 import { entityRegistry } from '../registry'
 import { FootBot } from './FootBot'
 import { KheperaIV } from './KheperaIV'
+import { LeoRenderer } from './LeoRenderer'
 import { BoxRenderer } from './BoxRenderer'
 import { CylinderRenderer } from './CylinderRenderer'
 import { LightRenderer } from './LightRenderer'
@@ -8,6 +9,7 @@ import { FloorRenderer } from './FloorRenderer'
 
 entityRegistry.register('foot-bot', FootBot)
 entityRegistry.register('kheperaiv', KheperaIV)
+entityRegistry.register('Leo', LeoRenderer)
 entityRegistry.register('box', BoxRenderer)
 entityRegistry.register('cylinder', CylinderRenderer)
 entityRegistry.register('light', LightRenderer)
